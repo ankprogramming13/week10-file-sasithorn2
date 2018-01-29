bfb3aeaab36eaf3efeaf2eb70b382b73cfb5ec3c
Unknown <toeyhom64@gmail.com>
2018-01-29 19:45:16 +0700

    10.2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,21 @@
-int main() {
-        /* code */
-        return 0;
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+    string line;
+    ifstream myfile ("example.txt");
+    if (myfile.is_open())
+    {
+        while (getline(myfile,line))
+    {
+        cout <<line << '\n';
+
+    }
+    myfile.close();
+
+}
+    else cout << "Unable to open file";
+    return 0;
 }
